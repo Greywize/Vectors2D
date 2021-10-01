67d122d1ada558f75a38214efde8fde7abe5946d
Greywize <68831304+Greywize@users.noreply.github.com>
2021-10-02 02:17:26 +0930

    TweenBrain now calls UnityEvents when all tweens are done for tweening In & Out - Will be sper useful in the future

--- Assets/2_Scripts/Interface/TweenBrain.cs
+++ Assets/2_Scripts/Interface/TweenBrain.cs
@@ -1,18 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class TweenBrain : MonoBehaviour
 {
     // The tween brain
     // Calls all tween functions on this gameObject
-    // Required by all tweens
 
     // Call TweenIn & TweenOut from other scripts to call all tweens for this gameObject at once
 
     public delegate void TweenEvent();
     public TweenEvent tweenOut;
     public TweenEvent tweenIn;
+
+    public UnityEvent onTweenOutComplete;
+    public UnityEvent onTweenInComplete;
 
     public void TweenOut()
     {
--- Assets/2_Scripts/Interface/UITween.cs
+++ Assets/2_Scripts/Interface/UITween.cs
@@ -26,33 +26,35 @@ public class UITween : MonoBehaviour
         Rotation
     }
 
+    #region Tween Values
     [Min(0)] [Tooltip("How long the tween will last (in seconds)")]
     public float tweenTime = 0.5f;
-
     [SerializeField] [Range(0, 1)]
-    private float alphaOut = 0;
+    private float alphaOut;
     [SerializeField] [Range(0, 1)]
     private float alphaIn = 1;
-
     public bool uniformScale = true;
     [SerializeField]
-    private float scaleOut = 0;
+    private float scaleOut;
     [SerializeField]
     private Vector2 scaleOutVector;
     [SerializeField]
     private float scaleIn = 1;
     [SerializeField]
     private Vector2 scaleInVector = Vector2.one;
-
     [SerializeField]
     private Vector2 positionOut;
     [SerializeField]
     private Vector2 positionIn;
-
     [SerializeField] [Tooltip("Rotation to tween out (in degrees)")]
     private float rotationOut;
     [SerializeField] [Tooltip("Rotation to tween in (in degrees)")]
     private float rotationIn;
+    #endregion
+    #region Internal Logic Variables
+    public bool completed = false;
+    private bool tweenOut = false;
+    #endregion
 
     private void OnEnable()
     {
@@ -69,6 +71,9 @@ public class UITween : MonoBehaviour
 
     private void TweenIn()
     {
+        completed = false;
+        tweenOut = false;
+
         if (tweenTime <= 0)
         {
             Debug.LogWarning($"A tween time of zero or less on {gameObject.name} won't do anything!");
@@ -101,6 +106,9 @@ public class UITween : MonoBehaviour
     }
     private void TweenOut()
     {
+        completed = false;
+        tweenOut = true;
+
         if (tweenTime <= 0)
         {
             Debug.LogWarning($"A tween time of zero or less on {gameObject.name} won't do anything!");
@@ -134,6 +142,7 @@ public class UITween : MonoBehaviour
 
     private void TweenAlpha(float alpha)
     {
+        // Null checks
         if (!canvasGroup)
         {
             canvasGroup = GetComponent<CanvasGroup>();
@@ -154,10 +163,12 @@ public class UITween : MonoBehaviour
         }
         if (!canvas.enabled)
             canvas.enabled = true;
-        LeanTween.alphaCanvas(canvasGroup, alpha, tweenTime).setEase(tweenType).setOnComplete(() => { if (canvasGroup.alpha == 0) { canvas.enabled = false; } }); ;
+        LeanTween.alphaCanvas(canvasGroup, alpha, tweenTime).setEase(tweenType)
+            .setOnComplete(() => { if (canvasGroup.alpha == 0) { canvas.enabled = false; } CheckComplete(tweenOut); });
     }
     private void TweenScale(float scale)
     {
+        // Null checks
         if (!canvas)
         {
             canvas = GetComponent<Canvas>();
@@ -178,18 +189,60 @@ public class UITween : MonoBehaviour
         }
         if (!canvas.enabled)
             canvas.enabled = true;
-        LeanTween.scale(gameObject, Vector3.one * scale, tweenTime).setEase(tweenType).setOnComplete(() => { if (rect.localScale.magnitude == 0) { canvas.enabled = false; } });
+        LeanTween.scale(gameObject, Vector3.one * scale, tweenTime).setEase(tweenType)
+            .setOnComplete(() => { if (rect.localScale.magnitude == 0) { canvas.enabled = false; } CheckComplete(tweenOut); });
     }
     private void TweenScale(Vector2 scale)
     {
-        LeanTween.scale(gameObject, scale, tweenTime).setEase(tweenType);
+        if (!canvas)
+        {
+            canvas = GetComponent<Canvas>();
+            if (canvas == null)
+            {
+                Debug.LogWarning($"A Canvas is required to tween scale, but it was missing on {gameObject.name}.");
+                return;
+            }
+        }
+        if (!rect)
+        {
+            rect = GetComponent<RectTransform>();
+            if (canvas == null)
+            {
+                Debug.LogWarning($"A RectTransform is required to tween scale, but it was missing on {gameObject.name}.");
+                return;
+            }
+        }
+        LeanTween.scale(gameObject, scale, tweenTime).setEase(tweenType)
+            .setOnComplete(() => { if (rect.localScale.magnitude == 0) { canvas.enabled = false; } CheckComplete(tweenOut); });
     }
     private void TweenPosition(Vector2 position)
     {
-        LeanTween.moveLocal(gameObject, position, tweenTime).setEase(tweenType);
+        LeanTween.moveLocal(gameObject, position, tweenTime).setEase(tweenType)
+            .setOnComplete(() => CheckComplete(tweenOut));
     }
     private void TweenRotation(float rotation)
     {
-        LeanTween.rotateAroundLocal(gameObject, new Vector3(0, 0, 1), rotation, tweenTime).setEase(tweenType);
+        LeanTween.rotateAroundLocal(gameObject, new Vector3(0, 0, 1), rotation, tweenTime).setEase(tweenType)
+            .setOnComplete(() => CheckComplete(tweenOut));
+    }
+
+    private void CheckComplete(bool tweeningOut)
+    {
+        // We only call this funtion if this tween is completed
+        completed = true;
+
+        foreach (UITween tween in GetComponents<UITween>())
+        {
+            if (!tween.completed)
+                return;
+        }
+
+        if (!tweenBrain)
+            tweenBrain = GetComponent<TweenBrain>();
+
+        if (tweeningOut)
+            tweenBrain.onTweenOutComplete?.Invoke();
+        else
+            tweenBrain.onTweenInComplete?.Invoke();
     }
 }
