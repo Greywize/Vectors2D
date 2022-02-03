a520e2cd75da1fe15be258b260f47f2d88bc75f9
Greywize <68831304+Greywize@users.noreply.github.com>
2022-02-04 01:39:03 +1030

    More vector math - unfinished - Dragging an object now pulls them from the point at which you clicked on the object instead of the object's center - Work in progress torque applied when the object is pulled

--- Assets/IdleClickDrag.cs
+++ Assets/IdleClickDrag.cs
@@ -8,10 +8,14 @@ using UnityEngine.InputSystem;
 public class IdleClickDrag : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IInitializePotentialDragHandler
 {
     Rigidbody2D rigidBody;
+    RectTransform rectTransform;
 
     InputAction mouseInput;
     Vector2 mousePosition;
+    Vector2 pivotOffset;
+    Vector2 grabPoint;
     float distance;
+    Vector2 upDirectionOnGrab;
 
     [SerializeField] [Min(0)]
     [Tooltip("The minimum distance away from the pointer the object needs to be before elasticity kicks in")]
@@ -27,6 +31,7 @@ public class IdleClickDrag : MonoBehaviour, IPointerDownHandler, IPointerUpHandl
     private void Start()
     {
         rigidBody = GetComponent<Rigidbody2D>();
+        rectTransform = GetComponent<RectTransform>();
 
         mouseInput = InputManager.interfaceActions.FindAction("Point");
         mouseInput.Enable();
@@ -34,24 +39,26 @@ public class IdleClickDrag : MonoBehaviour, IPointerDownHandler, IPointerUpHandl
 
     private void FixedUpdate()
     {
-        if (!dragging)
-            return;
-
         mousePosition = mouseInput.ReadValue<Vector2>();
         mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
 
         distance = Vector2.Distance(mousePosition, transform.position);
-        if (distance > minDistance)
+        if (dragging && distance > minDistance)
         {
-            Vector3 dragDirection = new Vector2(mousePosition.x - transform.position.x,
-                                                mousePosition.y - transform.position.y);
+            grabPoint = (Vector2)transform.position + pivotOffset;
+            float angle = Vector2.Angle(upDirectionOnGrab, transform.up) * Mathf.Deg2Rad;
 
-            rigidBody.velocity += (Vector2)dragDirection * elasticity;
+            Vector2 adjustedGrabPoint = grabPoint - (Vector2)transform.position;
+            adjustedGrabPoint = (Vector2)transform.position + new Vector2(adjustedGrabPoint.x * Mathf.Cos(angle) - adjustedGrabPoint.y * Mathf.Sin(angle),
+                                            adjustedGrabPoint.x * Mathf.Sin(angle) + adjustedGrabPoint.y * Mathf.Cos(angle));
 
+            Vector2 dragDirection = mousePosition - adjustedGrabPoint;
+            rigidBody.velocity += dragDirection * elasticity;
+
+            // Get the closest point to the perimeter of the maxDistance radius
+            Vector3 distanceToPerimiter = dragDirection.normalized * (distance - maxDistance);
             if (distance > maxDistance)
             {
-                // Get the closest point to the perimeter of the maxDistance radius
-                Vector3 distanceToPerimiter = dragDirection.normalized * (distance - maxDistance);
                 // Correct our position by adding distanceToPerimiter to our position
                 transform.position += distanceToPerimiter;
                 // Maintain velocity of correction movement
@@ -62,6 +69,14 @@ public class IdleClickDrag : MonoBehaviour, IPointerDownHandler, IPointerUpHandl
                 if (dot > 0)
                     rigidBody.velocity -= component;
             }
+
+            Vector2 r = adjustedGrabPoint - (Vector2)transform.position;
+
+            float torque = r.magnitude * dragDirection.magnitude * Mathf.Sin(r.magnitude);
+
+            Debug.DrawLine(adjustedGrabPoint, mousePosition, Color.white);
+
+            rigidBody.AddTorque(torque);
         }
     }
 
@@ -72,6 +87,9 @@ public class IdleClickDrag : MonoBehaviour, IPointerDownHandler, IPointerUpHandl
     public void OnPointerDown(PointerEventData eventData)
     {
         dragging = true;
+
+        pivotOffset = mousePosition - (Vector2)transform.position;
+        upDirectionOnGrab = transform.up;
     }
     public void OnPointerUp(PointerEventData eventData)
     {
